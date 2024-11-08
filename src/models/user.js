fe40b485968cdc21f35c241fcce9be638a0ebb3d
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-08 09:52:43 +0100

    user model completed

--- src/models/user.js
+++ src/models/user.js
@@ -28,3 +28,81 @@ const passwordEncrypt = (password) => {
 }
 
 /* ------------------------------------------------------- */
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: [true, "Email is required!"],
+      // validate: (email) => {
+      //     if (email.includes('@') && email.includes('.')) {
+      //         return true
+      //     } else {
+      //         return false
+      //     }
+      // },
+      // validate: (email)=> (email.includes('@') && email.includes('.')),
+      validate: [
+        (email) => email.includes("@") && email.includes("."),
+        "Email type is incorrect!",
+      ],
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+
+      /* ------------------------------------------------------- *
+
+        set: (password) => { 1234 --> 331247687vd7345f3hds687234gdf76382532g3tvdf
+            return 'customPassword'
+        },
+        validate: (password) => {
+            if (password.length < 5) return false
+            else true
+        },
+
+        /* ------------------------------------------------------- */
+
+      set: (password) => {
+        // set runs before validate
+        if (password.length < 5) {
+          // throw new Error('Invalid Password!') // Syntax Error
+          return "InvalidPassword";
+        } else {
+          return passwordEncrypt(password);
+        }
+      },
+      validate: (password) => {
+        if (password === "InvalidPassword") {
+          return false;
+        } else {
+          return true;
+        }
+      },
+      /* ------------------------------------------------------- */
+      // set: (password) => (password.length > 5 ? passwordEncrypt(password) : 'InvalidPassword'),
+      // validate: (password) => password != 'InvalidPassword'
+    },
+
+    userName: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+
+    firstName: String,
+
+    lastName: String,
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", UserSchema);
